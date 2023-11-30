968e41eb9dcd9b05b3d1cb224eb0114a26ce86b8
keiko <kjoceliandita@gmail.com>
2023-11-30 17:18:45 +0700

    First commit

--- app/src/main/java/com/example/rizzler/LoginActivity.kt
+++ app/src/main/java/com/example/rizzler/LoginActivity.kt
@@ -1,11 +1,79 @@
 package com.example.rizzler
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        forgetpassword.setOnClickListener {
+            startActivity(Intent(this,ForgetActivity::class.java))
+        }
+        login_btn.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email = login_email.text.toString()
+        val password = login_password.text.toString()
+
+        when {
+            TextUtils.isEmpty(email) -> Toast.makeText(
+                this,
+                "Username is required",
+                Toast.LENGTH_SHORT
+            ).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(
+                this,
+                "Password is required",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this@LoginActivity)
+                progressDialog.setTitle("Login")
+                progressDialog.setMessage("Logging in...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            progressDialog.dismiss()
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            val message = task.exception!!.toString()
+                            Toast.makeText(this, "Password or Email Invalid", Toast.LENGTH_LONG).show()
+                            mAuth.signOut()
+                            progressDialog.dismiss()
+                        }
+                    }
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(FirebaseAuth.getInstance().currentUser!=null)
+        {
+            //forwarding to home page
+            val intent= Intent(this@LoginActivity,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
     }
 }
